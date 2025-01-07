8864de723eb210ebf5714a58e7ec36f55a0bf12e
connor truex <connor@mellicell.com>
2025-01-06 19:59:38 -0500

    added customer table

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,28 @@
 import { Metadata } from 'next';
+import CustomersTable from '@/app/ui/customers/table';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+// import { FormattedCustomersTable } from '@/app/lib/definitions';
+import { lusitana } from '@/app/ui/fonts';
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default function Page() {
-    return <p>Customers Page</p>;
+export default async function Page(props: {
+    searchParams: Promise<{
+      query?: string;
+      page?: string
+    }>}) {
+    const searchParams = await props.searchParams;
+    const query = searchParams?.query || '';
+    const currentPage = Number(searchParams?.page) || 1;
+
+    const customers = await fetchFilteredCustomers(query);
+
+    return (
+        <div>
+            <CustomersTable customers={customers} />
+        </div>
+    );
+
 }
