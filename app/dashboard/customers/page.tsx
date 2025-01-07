d128ddfe19db45ff02cdd4455e2d5be04208d93f
connor truex <connor@mellicell.com>
2025-01-06 20:02:08 -0500

    fixed linting issues

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -2,7 +2,6 @@ import { Metadata } from 'next';
 import CustomersTable from '@/app/ui/customers/table';
 import { fetchFilteredCustomers } from '@/app/lib/data';
 // import { FormattedCustomersTable } from '@/app/lib/definitions';
-import { lusitana } from '@/app/ui/fonts';
 
 export const metadata: Metadata = {
   title: 'Customers',
@@ -11,11 +10,9 @@ export const metadata: Metadata = {
 export default async function Page(props: {
     searchParams: Promise<{
       query?: string;
-      page?: string
     }>}) {
     const searchParams = await props.searchParams;
     const query = searchParams?.query || '';
-    const currentPage = Number(searchParams?.page) || 1;
 
     const customers = await fetchFilteredCustomers(query);
 
